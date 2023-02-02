12b61b7f2573eded3b801d2fe5c73ea2c7c62488
Charlie <charlottevfarmer@gmail.com>
2023-02-02 13:42:27 +0000

    Add routes for main pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 // import './App.css';
 import styles from './App.module.css'
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route
+            exact
+            path='/'
+            render={()=><h1>Test home</h1>}
+          />
+          <Route
+            exact
+            path='/favourites'
+            render={()=><h1>Test favourites</h1>}
+          />
+          <Route
+            exact
+            path='/following'
+            render={()=><h1>Test following</h1>}
+          />
+          <Route
+            exact
+            path='/signin'
+            render={()=><h1>Test signin</h1>}
+          />
+          <Route
+            exact
+            path='/signup'
+            render={()=><h1>Test signup</h1>}
+          />
+          <Route
+            render={()=><p>Page not found!</p>}
+          />
+        </Switch>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Container from 'react-bootstrap/Container'
+import { NavLink } from 'react-router-dom';
 import logo from "../assets/ei-miko-cooking.png";
 import styles from '../styles/NavBar.module.css'
 
@@ -13,34 +14,63 @@ const NavBar = () => {
             fixed="top"
         >
             <Container>
-                <Navbar.Brand>
-                    <img src={logo} alt="logo" height="55" />
-                    Genshin Recipes Blog
-                </Navbar.Brand>
+                <NavLink to="/">
+                    <Navbar.Brand>
+                        <img src={logo} alt="logo" height="45" />
+                        Genshin Recipes Blog
+                    </Navbar.Brand>
+                </NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
-                        <Nav.Link>
+                        <NavLink
+                            exact
+                            to="/"
+                            className={styles.NavLink}
+                            activeClassName={styles.Active}
+                        >
                             <i className="fas fa-home"></i>
                             Home
-                        </Nav.Link>
-                        <Nav.Link>
-                            <i class="fa-solid fa-star"></i>
+                        </NavLink>
+                        <NavLink
+                            to="/favourites"
+                            className={styles.NavLink}
+                            activeClassName={styles.Active}
+                        >
+                            <i className="fa-solid fa-star"></i>
                             Favourites
-                        </Nav.Link>
-                        <Nav.Link>
-                            <i class="fa-solid fa-address-book"></i>
+                        </NavLink>
+                        <NavLink
+                            to="/following"
+                            className={styles.NavLink}
+                            activeClassName={styles.Active}
+                        >
+                            <i className="fa-solid fa-address-book"></i>
                             Following
-                        </Nav.Link>
-                        <Nav.Link>
+                        </NavLink>
+                        <NavLink
+                            to="/signin"
+                            className={styles.NavLink}
+                            activeClassName={styles.Active}
+                        >
                             <i className="fas fa-sign-in-alt"></i>
                             Sign In
-                        </Nav.Link>
-                        <Nav.Link>
+                        </NavLink>
+                        <NavLink
+                            to="/signup"
+                            className={styles.NavLink}
+                            activeClassName={styles.Active}
+                        >
                             <i className="fas fa-user-plus"></i>
                             Sign Up
-                        </Nav.Link>
-                        <Nav.Link>Profile</Nav.Link>
+                        </NavLink>
+                        <NavLink
+                            to="/"
+                            className={styles.NavLink}
+                            activeClassName={styles.Active}
+                        >
+                            Profile
+                        </NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
