b93ccd3ff9a6532622f691100b1acf33f0d8ab48
Charlie <charlottevfarmer@gmail.com>
2023-02-03 10:28:26 +0000

    SignInForm setup

--- src/App.js
+++ src/App.js
@@ -5,8 +5,13 @@ import Container from 'react-bootstrap/Container';
 import { Route, Switch } from 'react-router-dom';
 import './api/axiosDefaults'
 import SignUpForm from './pages/auth/SignUpForm';
+import SignInForm from './pages/auth/SignInForm';
+import { useState } from 'react';
 
 function App() {
+
+  const [currentUser, setCurrentUser] = useState(null)
+
   return (
     <div className={styles.App}>
       <NavBar />
@@ -15,30 +20,30 @@ function App() {
           <Route
             exact
             path='/'
-            render={()=><h1>Test home</h1>}
+            render={() => <h1>Test home</h1>}
           />
           <Route
             exact
             path='/favourites'
-            render={()=><h1>Test favourites</h1>}
+            render={() => <h1>Test favourites</h1>}
           />
           <Route
             exact
             path='/following'
-            render={()=><h1>Test following</h1>}
+            render={() => <h1>Test following</h1>}
           />
           <Route
             exact
             path='/signin'
-            render={()=><h1>Test signin</h1>}
+            render={() => <SignInForm />}
           />
           <Route
             exact
             path='/signup'
-            render={()=><SignUpForm />}
+            render={() => <SignUpForm />}
           />
           <Route
-            render={()=><p>Page not found!</p>}
+            render={() => <p>Page not found!</p>}
           />
         </Switch>
       </Container>
